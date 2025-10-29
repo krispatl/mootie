f4410c7209bf18839fd1ec9a85382a1bb9baa17e
krispatl <31904924+krispatl@users.noreply.github.com>
2025-10-28 20:33:40 -0400

    Delete api/health.js

--- api/health.js
+++
@@ -1,9 +0,0 @@
-// api/health.js
-export default async function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    time: new Date().toISOString(),
-    commit: process.env.VERCEL_GIT_COMMIT_SHA || null,
-    url: process.env.VERCEL_URL || null
-  });
-}
